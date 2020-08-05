c45758eeb7288b27b85114652a2774fb78d6f43f
didu <azoulai.edouard@gmail.com>
2020-08-05 23:47:47 +0100

    Add first api call (get recent orders on a specific pair).

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const axios = require('axios');
+const axios = require('axios')
+
+const orders_url = 'https://api.kraken.com/0/public/Depth'
+const trades_url = 'https://api.kraken.com/0/public/Trades'
+const pair = 'ETHUSD'
+
+async function getOrders() {
+    try {
+        const { data } = await axios.get(orders_url + `?pair=${pair}`)
+        if (data.error && data.error.length !== 0) {
+            console.error(data.error)
+        }
+        console.log(data.result)
+    } catch (error) {
+        console.error(error)
+  }
+}
+
+getOrders()
